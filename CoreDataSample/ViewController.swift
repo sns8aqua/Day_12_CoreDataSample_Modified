27649a3757b897c2ffb7d8214efd2f958ddc071a
Santhosh <clicomdeveloper@gmail.com>
2022-01-23 22:06:33 +0530

    Initial Commit

---
+++ CoreDataSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by Santhosh on 23/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
